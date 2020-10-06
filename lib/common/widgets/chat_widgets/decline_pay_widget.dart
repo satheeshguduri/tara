fa2b2b720a1d21d4d7587d74dc83786c31efa0be
Kiran  Yasala <kirankumar.y@abhibus.com>
2020-10-06 12:54:17 +0530

    Modified the code regarding set the design for download icon in decline_pay_widget file

--- lib/common/widgets/chat_widgets/decline_pay_widget.dart
+++ lib/common/widgets/chat_widgets/decline_pay_widget.dart
@@ -39,142 +39,153 @@ class _DeclinePayState extends State<DeclinePay> {
                 spreadRadius: 0)
           ],
           color: AppColors.primaryBackground),
-      child:Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child:Stack(
         children: [
-         Row(
-           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-           children: [
-             Column(
-               children: [
-                 Container(
-                   height: 32,
-                   child: Align(
-                     alignment: Alignment.centerLeft,
-                     child: Container(
-                       margin: EdgeInsets.only(right: 8,top: 12,bottom: 4),
-                       child: Text(
-                         Strings.TANIA_REQUESTED,
-                         textAlign: TextAlign.left,
-                         style: BaseStyles.agentConfirmedTextStyle,
-                       ),
-                     ),
-                   ),
-                 ),
-                 Container(
-                   margin: EdgeInsets.only(left: 16,right: 8,bottom: 8),
-                   child:Text(
-                       "Rp 100.000",
-                       style: BaseStyles.agentUIN_OTP_CODE_TextStyle
-                   ),
-                 ),
-               ],
-             ),
-             // Oval
-             Container(
-               margin: EdgeInsets.only(right: 8,),
-               child: Opacity(
-                 opacity : 0.6,
-                 child: Container(
-                     width: 46,
-                     height: 46,
-                     decoration: BoxDecoration(
-                         gradient: LinearGradient(
-                             begin: Alignment(0.9999999999999998, 0.49999999999999983),
-                             end: Alignment(-2.220446049250313e-16, 0.5000000000000002),
-                             colors: [Color(0xffb2f7e2), const Color(0xffa1f0f8)])
-                     )
-                 ),
-               ),
-             )
-           ],
-         ),
-
-          Container(
-            margin: EdgeInsets.only(left: 16,right: 16),
-            child: Container(
-                height: 1,
-                decoration: BoxDecoration(
-                    color: Color(0xffe9ecef)
-                )
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 16,right: 16,top: 8),
-            child: Text(
-                "“Buat patungan kado Bambang”",
-                style: BaseStyles.saveToMyContactTextStyle
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 16,right: 16,top: 8,bottom: 8),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 5,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 32,
+                child: Align(
+                  alignment: Alignment.centerLeft,
                   child: Container(
-                    padding:  EdgeInsets.only(bottom: 8,top: 8),
-                  margin: EdgeInsets.only(right: 8),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(8)
-                        ),
-                        border: Border.all(
-                            color: const Color(0xffb0b4c1),
-                            width: 1
-                        ),
-                        color: const Color(0xffffffff)
+                    margin: EdgeInsets.only(right: 8,top: 12,bottom: 4,left: 16),
+                    child: Text(
+                      Strings.TANIA_REQUESTED,
+                      textAlign: TextAlign.left,
+                      style: BaseStyles.agentConfirmedTextStyle,
                     ),
-                    child: Center(
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 16,right: 8,bottom: 8),
+                child:Text(
+                    "Rp 100.000",
+                    style: BaseStyles.agentUIN_OTP_CODE_TextStyle
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(left: 16,right: 16),
+                child: Container(
+                    height: 1,
+                    decoration: BoxDecoration(
+                        color: Color(0xffe9ecef)
+                    )
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 16,right: 16,top: 8),
+                child: Text(
+                    "“Buat patungan kado Bambang”",
+                    style: BaseStyles.saveToMyContactTextStyle
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 16,right: 16,top: 8,bottom: 8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Container(
+                        padding:  EdgeInsets.only(bottom: 8,top: 8),
+                        margin: EdgeInsets.only(right: 8),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(8)
+                            ),
+                            border: Border.all(
+                                color: const Color(0xffb0b4c1),
+                                width: 1
+                            ),
+                            color: const Color(0xffffffff)
+                        ),
+                        child: Center(
+                          child: Text(
+                              Strings.DECLINE,
+                              style: BaseStyles.chatItemButtonTextStyle
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: Container(
+                        padding:  EdgeInsets.only(bottom: 8,top: 8),
+                        margin: EdgeInsets.only(left: 8,),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(8)
+                            ),
+                            border: Border.all(
+                                color: const Color(0xffb2f7e2),
+                                width: 1
+                            ),
+                            color: const Color(0xffb2f7e2)
+                        ),
+                        child: Center(
+                          child: Text(
+                              Strings.Pay,
+                              style: BaseStyles.chatItemButtonTextStyle
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
                       child: Text(
-                          Strings.DECLINE,
-                          style: BaseStyles.chatItemButtonTextStyle
+                        DateFormat('dd MMM kk:mm')
+                            .format(DateTime.fromMillisecondsSinceEpoch(1565888474278)),
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12.0,
+                            fontStyle: FontStyle.normal),
+                      ),
+                      margin: EdgeInsets.only(bottom:8.0,right: 8),
+                    )])
+            ],
+          ),
+          Container(
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Opacity(
+                opacity : 0.6,
+                child: Container(
+                    width: 46,
+                    height: 46,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment(0.9999999999999998, 0.49999999999999983),
+                            end: Alignment(-2.220446049250313e-16, 0.5000000000000002),
+                            colors: [Color(0xffb2f7e2), const Color(0xffa1f0f8)]),
+                        borderRadius: new BorderRadius.only(
+                            bottomLeft: const Radius.circular(40.0),
+                            )
+                    ),
+                  child: Container(
+                    margin: EdgeInsets.only(right: 8,bottom: 4,),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Image.asset(
+                        "assets/images/icon-20.png",
+                        fit: BoxFit.fill,
+                        color: AppColors.header_top_bar_color,
+                        width: 24, height: 24,
                       ),
                     ),
                   ),
                 ),
-                Expanded(
-                  flex: 5,
-                  child: Container(
-                    padding:  EdgeInsets.only(bottom: 8,top: 8),
-                  margin: EdgeInsets.only(left: 8,),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(8)
-                        ),
-                        border: Border.all(
-                            color: const Color(0xffb2f7e2),
-                            width: 1
-                        ),
-                        color: const Color(0xffb2f7e2)
-                    ),
-                    child: Center(
-                      child: Text(
-                          Strings.Pay,
-                          style: BaseStyles.chatItemButtonTextStyle
-                      ),
-                    ),
-                  ),
-                )
-              ],
+              ),
             ),
-          ),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Container(
-                  child: Text(
-                    DateFormat('dd MMM kk:mm')
-                        .format(DateTime.fromMillisecondsSinceEpoch(1565888474278)),
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 12.0,
-                        fontStyle: FontStyle.normal),
-                  ),
-                  margin: EdgeInsets.only(bottom:8.0,right: 8),
-                )])
+          )
         ],
-      ),
+      )
     );
   }
 
