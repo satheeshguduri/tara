fd086fbe05c8e37b68ab0a2c49cbd4d1976410e5
harish chandra <harishpabbathi@gmail.com>
2020-12-17 23:15:57 +0530

    Committed Missing File

---
+++ lib/models/order_management/orders/order_extra_data.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'order_extra_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+OrderExtraData _$OrderExtraDataFromJson(Map<String, dynamic> json) {
+  return OrderExtraData(
+    customer_commid: json['customer_commid'] as String,
+    merchant_commid: json['merchant_commid'] as String,
+    interpret: json['interpret'] as bool,
+  );
+}
+
+Map<String, dynamic> _$OrderExtraDataToJson(OrderExtraData instance) =>
+    <String, dynamic>{
+      'customer_commid': instance.customer_commid,
+      'merchant_commid': instance.merchant_commid,
+      'interpret': instance.interpret,
+    };
